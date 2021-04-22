287780dfa113e7da49a625250653273eb34dccc3
Jinsu Park <bo314@naver.com>
2021-04-22 15:22:24 +0900

    Initial beanstalk

---
+++ script/execute.sh
@@ -0,0 +1,2 @@
+cd web
+python app.py
---
+++ web/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+# print a nice greeting.
+@app.route('/')
+def say_hello():
+    return '<p>Hello!</p>\n'
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
