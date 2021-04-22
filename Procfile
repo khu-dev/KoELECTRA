9b0e0684fa64cdf539561bd1a462abe949dec64b
Jinsu Park <bo314@naver.com>
2021-04-22 16:55:53 +0900

    Dummy process

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: ./script/execute.sh
+dummy_worker: PORT=9999 python web/app.py
--- web/app.py
+++ web/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 # print a nice greeting.
@@ -9,6 +11,5 @@ def say_hello():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
     # app.run('0.0.0.0', port=8000)
-    app.run(port=8000)
+    app.run(host= '0.0.0.0', debug=True, port=int(os.getenv("PORT", "8000")))
